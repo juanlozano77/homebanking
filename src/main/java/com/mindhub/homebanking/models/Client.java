d323a641dddb69e9c945afc7f8ef65d3bbe49376
Juan L <juanjavierlozano@hotmail.com>
2023-08-23 19:07:28 -0300

    completado inicio de sesión y registro

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,88 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private String password;
+    @OneToMany(mappedBy="client", fetch=FetchType.EAGER)
+    private Set<Account> accounts = new HashSet<>();
+
+    @OneToMany(mappedBy = "client", fetch = FetchType.EAGER)
+    private Set<ClientLoan> clientLoans = new HashSet<>();
+    @OneToMany(mappedBy="client", fetch=FetchType.EAGER)
+    private Set<Card> cards = new HashSet<>();
+    private boolean isAdmin;
+    public Client() { }
+    public Client (String first, String last,String email, String password) {
+        this.firstName = first;
+        this.lastName = last;
+        this.email=email;
+        this.password=password;
+    }
+
+    public long getId() {return id;}
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getEmail() {return email;}
+    public Set<Card> getCards() {return cards;}
+    public Set<ClientLoan> getClientLoans() {return clientLoans;}
+
+    public boolean isAdmin() {return isAdmin;}
+
+    public String getPassword() {return password;}
+
+    public void setAccounts(Set<Account> accounts) {this.accounts = accounts;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public void setAdmin(boolean admin) {isAdmin = admin;}
+
+    public void addAccount(Account account) {
+        account.setClient(this);
+        this.accounts.add(account);
+    }
+    public void addClientLoan(ClientLoan clientLoan) {
+        clientLoan.setClient(this);
+        clientLoans.add(clientLoan);
+    }
+
+    public void addCard(Card card) {
+        card.setClient(this);
+        this.cards.add(card);
+    }
+
+    public void setCards(Set<Card> cards) {this.cards = cards;}
+
+    public void setClientLoans(Set<ClientLoan> clientLoans) {
+        this.clientLoans = clientLoans;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
---
+++ src/main/java/com/mindhub/homebanking/models/Loan.java
@@ -0,0 +1,81 @@
+package com.mindhub.homebanking.models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+
+@Entity
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String name;
+    private double maxAmount;
+
+    @ElementCollection
+    @Column(name="payment")
+    private List<Integer> payments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "loan", fetch=FetchType.EAGER)
+    private Set<ClientLoan> clientLoans = new HashSet<>();
+    ;
+
+    public Loan(){
+
+    }
+
+    public Loan(String name, double maxAmount, List<Integer> payments) {
+        this.name = name;
+        this.maxAmount = maxAmount;
+        this.payments = payments;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getPayments() {
+        return payments;
+    }
+
+    public List<Client> getClients() {
+        return clientLoans.stream().map(ClientLoan::getClient).collect(toList());
+    }
+
+    public Set<ClientLoan> getClientLoans() {
+        return clientLoans;
+    }
+
+
+    public double getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMaxAmount(double maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public void setPayments(List<Integer> payments) {
+        this.payments = payments;
+    }
+
+
+    public void setClientLoans(Set<ClientLoan> clientLoans) {
+        this.clientLoans = clientLoans;
+    }
+}
+
